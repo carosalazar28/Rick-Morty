4ea458422f920ba5e64128fe138a57e6a7744555
Carolina Salazar <cindy.s.r.28@hotmail.com>
2020-09-30 16:29:45 -0500

    add gitignore and index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,32 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname,'dist'),
         filename: 'main.js'
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.js']
     },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                }
+    module:{
+        rules:[{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            use:{
+                loader: 'babel-loader'
             }
-        ]
-    }
+        }]
+    },
+    plugins: [
+        new HtmlWebPackPlugin([
+            {
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html',
+            }
+        ])
+    ]
+
 }
